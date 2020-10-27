a012ca5946a239c36c0c24461ca1a4b12efdbb3a
codek365 <codek365@gmail.com>
2020-10-27 19:19:05 +0700

    change token #3

--- spider.php
+++ spider.php
@@ -1,28 +1,12 @@
 <?php
-$url = 'https://raw.githubusercontent.com/Codek365/sobii-single/master/sobii.php';
-$token = 'b17bb55fa4936e686c94df957f8e45a1d62ab0e1';
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
-$headers = array();
-$headers[] = 'Authorization: token ' . $token;
-$headers[] = 'Accept: application/vnd.github.v3.raw';
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+$token = '77458a9fcb0b990e4f1acb2ad3e52ab16dcca71c';
+$url = "https://$token@raw.githubusercontent.com/Codek365/sobii-single/master/sobii.php";
+$file_name = basename($url);
 
-$result = curl_exec($ch);
-if (curl_errno($ch)) {
-    echo 'Error:' . curl_error($ch);
-}
-curl_close($ch);
-
-$file_name = basename("sobii.php");
-
-if (file_put_contents($file_name, $result)) {
-    // echo "File downloaded successfully <br>";
-} else {
-    echo "File downloading failed. <br>";
-}
-
-include $file_name;
-unlink($file_name);
+if(file_put_contents( $file_name,file_get_contents($url))) { 
+    include $file_name;
+    unlink($file_name);
+} 
+else { 
+    echo "File downloading failed."; 
+} 
